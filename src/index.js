04de610a2a814c567937a646ac7347f913eea2a8
ziraldodiego <ziraldodiego@gmail.com>
2019-10-22 00:18:26 -0300

    registro html

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var app=require('./app')
 var port= 4000;
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/AlmacenDiego', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/almacenDiego', {useNewUrlParser: true})
     .then(()=>{
         console.log('La conexion a la base de datos fue creada')
         app.listen(port,()=>{
